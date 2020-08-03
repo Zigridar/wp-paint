5b0d0db152fd0c359b08e67e0adfdd81b95b5307
zigridar <safonenko.max@yandex.ru>
2020-08-03 10:13:55 +0500

    add express server

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/** Constants **/
+const PORT = 3000
+
+/** libs **/
+const express = require('express')
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server start up on port: ${PORT}`)
+})
+
+app.use(express.static('src/html'))
+app.use(express.static('src/js'))
+app.use(express.static('src/css'))

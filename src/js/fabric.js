1c3db5bff538ad54d5921614f9de2a1a9a6bdd60
zigridar <safonenko.max@yandex.ru>
2020-08-06 12:18:07 +0500

    add zoom. autosize

--- src/js/fabric.js
+++ src/js/fabric.js
@@ -1,12 +1,25 @@
 'use strict'
 
-//todo auto size by window dimension
+/** global constants **/
+const WINDOW_SCALE_FACTOR = 0.95
+const HEIGHT = window.innerHeight * WINDOW_SCALE_FACTOR
+const WIDTH = window.innerWidth * WINDOW_SCALE_FACTOR
+const TOOLS_WIDTH = WIDTH / 3 /** 1/3 **/
+const EDITOR_WIDTH = WIDTH * 2 / 3 /** 2/3 **/
+
+const ELEMENTS =
+        {
+            planContainer: 'plan-container',
+            toolContainer: 'tools-container',
+            editorContainer: 'editor-container'
+        }
+
 
 $(document).ready(() => {
     /** create different boxes **/
-    createCanvas('plan-container', 'plan', 600, 400)
-    createCanvas('tools-container', 'tools', 600, 400)
-    createCanvas('editor-container', 'editor', 1000, 800)
+    createCanvas(ELEMENTS.planContainer, 'plan', TOOLS_WIDTH, HEIGHT / 2)
+    createCanvas(ELEMENTS.toolContainer, 'tools', TOOLS_WIDTH, HEIGHT / 2)
+    createCanvas(ELEMENTS.editorContainer, 'editor', EDITOR_WIDTH, HEIGHT)
 
     const plan = new fabric.Canvas('plan', {
         backgroundColor: 'rgb(250,82,201)'
@@ -21,6 +34,7 @@ $(document).ready(() => {
         backgroundColor: 'rgb(255,238,205)'
     })
 
+    //todo test
     const text = new fabric.Textbox('Kek', {
         width:250,
         cursorColor :"blue",
@@ -31,7 +45,6 @@ $(document).ready(() => {
         editable: false,
         selectable: false
     })
-
     const rect = new fabric.Rect({
         left: 150,
         top: 200,
@@ -44,32 +57,27 @@ $(document).ready(() => {
         editable: false,
         selectable: false
     })
-
-
     tools.add(text)
     tools.add(rect)
+    //todo test
 
-    setImgAsBackground(editor, 'map')
+    //todo add img utils
+    const backgroundImg = setImgAsBackground(editor, 'dog')
+    addCanvasHandlers(editor)
 
-    /** set all handlers using object notation **/
-    // tools.on({
-    //     'mouse:down': e => {
-    //         console.log(e.target)
-    //     }
-    // })
 })
 
 function createCanvas(containerId, canvasId, width, height) {
     $(`#${containerId}`).append(`<canvas id="${canvasId}" width="${width}" height="${height}"></canvas>`)
 }
 
-function createRect(width, height, position) {
+function createRect(position) {
     return new fabric.Rect({
         left: position.left,
-        top: position.right,
-        width: width,
-        height: height,
-        fill: 'rgba(255,0,0,0.5)',
+        top: position.top,
+        width: position.width,
+        height: position.height,
+        fill: 'rgba(255,0,0,0.5)', //todo
         transparentCorners: false,
         hasBorders: false,
         hasControls: false,
@@ -79,20 +87,49 @@ function createRect(width, height, position) {
 }
 
 function setImgAsBackground(fabricCanvas, imgId) {
-    const fabricImg = createImg(imgId)
-    const scaleFactor = Math.min(fabricCanvas.width / fabricImg.width, fabricCanvas.height / fabricImg.height)
-    fabricImg.scale(scaleFactor)
-    if (fabricImg.height < fabricCanvas.height) {
-        const top = (fabricCanvas.height - fabricImg.height) / 2
-        fabricImg.top = top
-    }
-    fabricCanvas.setBackgroundImage(fabricImg, fabricCanvas.renderAll.bind(fabricCanvas), {
-        originX: 'left',
-        originY: 'top'
+    const fabricImg = createImg(imgId, {
+        transparentCorners: false,
+        hasBorders: false,
+        hasControls: false,
+        editable: false,
+        selectable: false
     })
+    let scaleFactor = 1
+    /** scale image if its dimensions is bigger then dimensions of the canvas **/
+    if (fabricImg.width > fabricCanvas.width || fabricImg.height > fabricCanvas.height) {
+        scaleFactor = Math.min(fabricCanvas.width / fabricImg.width, fabricCanvas.height / fabricImg.height)
+        fabricImg.scale(scaleFactor)
+    }
+    /** set center by height **/
+    if (fabricImg.height * scaleFactor < fabricCanvas.height) {
+        fabricImg.top = (fabricCanvas.height - fabricImg.height * scaleFactor) / 2
+    }
+    /** set center by width **/
+    if (fabricImg.width * scaleFactor < fabricCanvas.width) {
+        fabricImg.left = (fabricCanvas.width - fabricImg.width * scaleFactor) / 2
+    }
+    fabricCanvas.add(fabricImg)
+    return fabricImg
 }
 
-function createImg(imgId, top, left) {
+/** create fabric image from <img ... /> **/
+function createImg(imgId, opt = {}) {
     const imgElement = document.getElementById(imgId)
-    return new fabric.Image(imgElement)
+    return new fabric.Image(imgElement, opt)
+}
+
+/** add handlers to canvas **/
+function addCanvasHandlers(fabricCanvas) {
+    /** zoom **/
+    fabricCanvas.on('mouse:wheel', opt => {
+        const delta = opt.e.deltaY
+        let zoom = fabricCanvas.getZoom()
+        zoom *= 0.999 ** delta
+        /** restrict zoom **/
+        if (zoom > 10) zoom = 10
+        if (zoom < 0.1) zoom = 0.1
+        fabricCanvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
+        opt.e.preventDefault()
+        opt.e.stopPropagation()
+    })
 }

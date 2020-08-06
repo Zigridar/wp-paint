217d3a04fd3966d4fc940f450cdd50cb1982a6eb
zigridar <safonenko.max@yandex.ru>
2020-08-06 18:07:37 +0500

    add elems to canvas

--- src/js/fabric.js
+++ src/js/fabric.js
@@ -10,60 +10,50 @@ const EDITOR_WIDTH = WIDTH * 2 / 3 /** 2/3 **/
 const ELEMENTS =
         {
             planContainer: 'plan-container',
-            toolContainer: 'tools-container',
-            editorContainer: 'editor-container'
+            toolContainer: 'tool-container',
+            editorContainer: 'editor-container',
+            plan: 'plan',
+            tools: 'tools',
+            editor: 'editor'
         }
+
+//todo get names from server and load using GET method
+const availableImages = ['bike.png', 'forest.png', 'stick.png']
+
+/** freeze const object **/
+Object.freeze(ELEMENTS)
 
 
 $(document).ready(() => {
     /** create different boxes **/
-    createCanvas(ELEMENTS.planContainer, 'plan', TOOLS_WIDTH, HEIGHT / 2)
-    createCanvas(ELEMENTS.toolContainer, 'tools', TOOLS_WIDTH, HEIGHT / 2)
-    createCanvas(ELEMENTS.editorContainer, 'editor', EDITOR_WIDTH, HEIGHT)
+    createCanvas(ELEMENTS.planContainer, ELEMENTS.plan, TOOLS_WIDTH, HEIGHT / 2)
+    createCanvas(ELEMENTS.editorContainer, ELEMENTS.editor, EDITOR_WIDTH, HEIGHT)
+
+    /** init draggable toolbox **/
+    const toolsContainer =  $(`#${ELEMENTS.toolContainer}`)
+    toolsContainer.css('height', HEIGHT / 2)
+    toolsContainer.css('max-width', TOOLS_WIDTH)
 
     const plan = new fabric.Canvas('plan', {
-        backgroundColor: 'rgb(250,82,201)'
-    })
-
-    const tools = new fabric.Canvas('tools', {
-        backgroundColor: 'rgb(222,250,205)',
-        selection: false
+        backgroundColor: 'rgb(198,197,250)'
     })
 
     const editor = new fabric.Canvas('editor', {
         backgroundColor: 'rgb(255,238,205)'
     })
 
-    //todo test
-    const text = new fabric.Textbox('Kek', {
-        width:250,
-        cursorColor :"blue",
-        top:10,
-        left:10,
-        hasBorders: false,
-        hasControls: false,
-        editable: false,
-        selectable: false
-    })
-    const rect = new fabric.Rect({
-        left: 150,
-        top: 200,
-        width: 150,
-        height: 120,
-        fill: 'rgba(255,0,0,0.5)',
-        transparentCorners: false,
-        hasBorders: false,
-        hasControls: false,
-        editable: false,
-        selectable: false
-    })
-    tools.add(text)
-    tools.add(rect)
-    //todo test
-
     //todo add img utils
-    const backgroundImg = setImgAsBackground(editor, 'dog')
-    addCanvasHandlers(editor)
+    const backgroundImg = setImgAsBackground(editor, 'map')
+    const editorContainer = document.getElementById(ELEMENTS.editorContainer)
+    /** add canvas handlers **/
+    addCanvasHandlers(editor, editorContainer)
+
+    const imageContainer = document.getElementById(ELEMENTS.toolContainer)
+
+    //todo server images
+    availableImages.forEach(imageSrc => {
+        addToolImage(imageContainer, imageSrc)
+    })
 
 })
 
@@ -71,23 +61,8 @@ function createCanvas(containerId, canvasId, width, height) {
     $(`#${containerId}`).append(`<canvas id="${canvasId}" width="${width}" height="${height}"></canvas>`)
 }
 
-function createRect(position) {
-    return new fabric.Rect({
-        left: position.left,
-        top: position.top,
-        width: position.width,
-        height: position.height,
-        fill: 'rgba(255,0,0,0.5)', //todo
-        transparentCorners: false,
-        hasBorders: false,
-        hasControls: false,
-        editable: false,
-        selectable: false
-    })
-}
-
 function setImgAsBackground(fabricCanvas, imgId) {
-    const fabricImg = createImg(imgId, {
+    const fabricImg = createImgFromId(imgId, {
         transparentCorners: false,
         hasBorders: false,
         hasControls: false,
@@ -113,13 +88,24 @@ function setImgAsBackground(fabricCanvas, imgId) {
 }
 
 /** create fabric image from <img ... /> **/
-function createImg(imgId, opt = {}) {
+function createImgFromId(imgId, opt = {}) {
     const imgElement = document.getElementById(imgId)
     return new fabric.Image(imgElement, opt)
 }
 
+/** create fabric image from HTML elemet **/
+function createImgFromElem(elem, x, y) {
+    const  fabricImg = new fabric.Image(elem, {
+        left: x,
+        top: y
+    })
+    const scaleFactor = Math.min(elem.width / elem.naturalWidth, elem.height / elem.naturalHeight)
+    fabricImg.scale(scaleFactor)
+    return fabricImg
+}
+
 /** add handlers to canvas **/
-function addCanvasHandlers(fabricCanvas) {
+function addCanvasHandlers(fabricCanvas, canvasContainer) {
     /** zoom **/
     fabricCanvas.on('mouse:wheel', opt => {
         const delta = opt.e.deltaY
@@ -132,4 +118,43 @@ function addCanvasHandlers(fabricCanvas) {
         opt.e.preventDefault()
         opt.e.stopPropagation()
     })
+
+    canvasContainer.ondrop = e => {
+        if (e.preventDefault)
+            e.preventDefault()
+        if (e.stopPropagation)
+            e.stopPropagation()
+
+        const pointer = fabricCanvas.getPointer(e, false)
+        const img = document.querySelector('img.img-dragging')
+        console.log(pointer.x - img.customOffsetX, pointer.y - img.customOffsetY)
+        const fabricImg = createImgFromElem(img, pointer.x - img.customOffsetX, pointer.y - img.customOffsetY)
+        fabricCanvas.add(fabricImg)
+    }
+
+    canvasContainer.ondragover = e => {
+        if (e.preventDefault) {
+            e.preventDefault()
+        }
+        e.dataTransfer.dropEffect = 'copy'
+    }
+}
+
+/** add tool image to toolbox **/
+function addToolImage(container, imageSrc) {
+    const img = document.createElement('img')
+    img.draggable = true
+    img.src = imageSrc
+    img.width = 60
+    img.height = 60
+    img.classList.add('tool-img')
+    img.ondragstart = e => {
+        img.classList.add('img-dragging')
+        img.customOffsetX = e.clientX - img.offsetLeft
+        img.customOffsetY = e.clientY - img.offsetTop
+    }
+    img.ondragend = e => {
+        img.classList.remove('img-dragging')
+    }
+    container.append(img)
 }
